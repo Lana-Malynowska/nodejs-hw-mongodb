11a851a968796c50fef2bc5566cb04cb2fa321d9
Svitlana Malynowska <lana.malynowska@gmail.com>
2025-06-15 18:53:19 +0300

    add delete router

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,6 +1,7 @@
 import createHttpError from 'http-errors';
 import {
   createContact,
+  deleteContact,
   getAllContacts,
   getContactById,
 } from '../services/contacts.js';
@@ -38,4 +39,15 @@ export const createContactController = async (req, res) => {
     message: 'Successfully created a contact!',
     data: contact,
   });
+};
+
+export const deleteContactController = async (req, res, next) => {
+  const { contactId } = req.params;
+  const contact = await deleteContact(contactId);
+
+  if (!contact) {
+    throw createHttpError(404, 'Contact not found');
+  }
+
+  res.status(204).send();
 };
