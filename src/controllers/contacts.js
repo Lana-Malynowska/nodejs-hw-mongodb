2a1b6205c3d50b19774b79343312895057a38b97
Svitlana Malynowska <lana.malynowska@gmail.com>
2025-06-15 15:56:29 +0300

    add error handling

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,6 +1,7 @@
+import createHttpError from 'http-errors';
 import { getAllContacts, getContactById } from '../services/contacts.js';
 
-export const getContactsController = async (req, res) => {
+export const getContactsController = async (req, res, next) => {
   const contacts = await getAllContacts();
 
   res.status(200).json({
@@ -10,16 +11,12 @@ export const getContactsController = async (req, res) => {
   });
 };
 
-export const getContactByIdController = async (req, res, next) => {
+export const getContactByIdController = async (req, res) => {
   const { contactId } = req.params;
   const contact = await getContactById(contactId);
 
   if (!contact) {
-    res.status(404).json({
-      status: 404,
-      message: 'Contact not found',
-    });
-    return;
+    throw createHttpError(404, 'Contact not found');
   }
 
   res.status(200).json({
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,3 +1,4 @@
+import createHttpError from 'http-errors';
 import { ContactsCollection } from '../db/models/contact.js';
 
 export const getAllContacts = async () => {
@@ -7,5 +8,10 @@ export const getAllContacts = async () => {
 
 export const getContactById = async (contactId) => {
   const contact = await ContactsCollection.findById(contactId);
+
+  if (!contact) {
+    throw createHttpError(404, 'Contact not found');
+  }
+
   return contact;
 };
