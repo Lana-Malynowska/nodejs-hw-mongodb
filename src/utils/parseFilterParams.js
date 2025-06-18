21eba9d2b033ad0b2bd25757dfd1f6d37600149f
Svitlana Malynowska <lana.malynowska@gmail.com>
2025-06-18 21:02:37 +0300

    add filtration

---
+++ src/utils/parseFilterParams.js
@@ -0,0 +1,25 @@
+const parseContactType = (type) => {
+  const isString = typeof type === 'string';
+  if (!isString) return;
+  const isContactType = (type) => ['work', 'home', 'personal'].includes(type);
+
+  if (isContactType(type)) return type;
+};
+
+const parseIsFavourite = (value) => {
+  if (value === 'true') return true;
+  if (value === 'false') return false;
+  return undefined;
+};
+
+export const parseFilterParams = (query) => {
+  const { type, isFavourite } = query;
+
+  const parsedType = parseContactType(type);
+  const parsedIsFavourite = parseIsFavourite(isFavourite);
+
+  return {
+    type: parsedType,
+    isFavourite: parsedIsFavourite,
+  };
+};
