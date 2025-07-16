1b97f41ec9e81c073b093d80a1360c9c01d4be3f
Svitlana Malynowska <lana.malynowska@gmail.com>
2025-07-16 12:31:19 +0300

    fix bugs in createContact

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -48,7 +48,24 @@ export const getContactByIdController = async (req, res, next) => {
 };
 
 export const createContactController = async (req, res, next) => {
-  const contact = await createContact({ ...req.body, userId: req.user._id });
+  const userId = req.user._id;
+  const photo = req.file;
+
+  let photoUrl;
+
+  if (photo) {
+    if (getEnvVar('ENABLE_CLOUDINARY') === 'true') {
+      photoUrl = await saveFileToCloudinary(photo);
+    } else {
+      photoUrl = await saveFileToUploadDir(photo);
+    }
+  }
+
+  const contact = await createContact({
+    ...req.body,
+    userId,
+    photo: photoUrl,
+  });
 
   res.status(201).json({
     status: 201,
